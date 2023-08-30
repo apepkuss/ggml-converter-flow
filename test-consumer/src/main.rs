9715dbfd3883856a17289de914adef6ebc8ad19f
Xin Liu <sam@secondstate.io>
2023-08-30 09:34:07 +0000

    chore(test-consumer): update code
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- test-consumer/src/main.rs
+++ test-consumer/src/main.rs
@@ -3,26 +3,56 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize)]
 struct ModelInfo {
-    name: String,
-    quant_info: String,
+    name: ModelType,
+    quant_info: QuantInfo,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+enum ModelType {
+    /// meta-llama/Llama-2-7b-hf
+    Llama2_7b,
+    /// meta-llama/Llama-2-7b-chat-hf
+    Llama2Chat7b,
+    /// LinkSoul/Chinese-Llama-2-7b
+    Llama2Chinese7b,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+enum QuantInfo {
+    /// q4_0
+    Q4,
+    /// q8_0
+    Q8,
+    /// f16
+    F16,
+    /// f32
+    F32,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct ConversionResult {
+    download_url: String,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let model_info = ModelInfo {
-        name: "meta-llama/Llama-2-7b-hf".to_string(),
-        quant_info: "q4_0".to_string(),
+        name: ModelType::Llama2_7b, // "meta-llama/Llama-2-7b-hf".to_string(),
+        quant_info: QuantInfo::Q4,  // "q4_0".to_string(),
     };
 
     let client = reqwest::Client::new();
 
-    let res = client
+    let response = client
         .post("http://localhost:3000/json")
         .json(&model_info)
         .send()
         .await?;
 
-    println!("{:?}", res);
+    println!("{:?}", response);
+
+    let conversion_result = response.json::<ConversionResult>().await?;
+    println!("download url: {}", conversion_result.download_url);
 
     Ok(())
 }
